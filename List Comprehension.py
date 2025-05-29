27c980ca61aa33d48684232dfbc2b321aafe4d03
Akshaj6 <akshaj.samaria@gmail.com>
2025-05-29 18:46:58 +0530

    -7

--- List Comprehension.py
+++ List Comprehension.py
@@ -4,13 +4,13 @@ try:
         print("Enter a number which is greater than 0 and positive")
     else:
         odd_numbers = [num for num in range(l) if num % 2 != 0]
-        print("List of odd numbers : {odd_numbers}")
+        print("List of odd numbers :", odd_numbers)
         list_odd = [num for num in range(1, l, 2)]
-        print("2nd list of odd numbers : {list_odd}")
+        print("2nd list of odd numbers :", list_odd)
 except ValueError:
     print("Invalid Input! Enter another number!")
 print("\n")
 fruits = ["apple", "banana", "mango", "pear", "dragonfruit", "strawberry", "guava"]
-print("List of fruits : {fruits}")
+print("List of fruits :", fruits)
 caps = [fruit.capitalize() for fruit in fruits]
-print("List with 1st letter capital : {caps}")
+print("List with 1st letter capital :", caps)
